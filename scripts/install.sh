158fdf13429cd8ca9004b6ae0362fbd3540158a6
sgoldsmith <sgoldsmith@dx4350>
2020-10-24 13:18:16 -0400

    Fix cron and add timestamp

--- scripts/install.sh
+++ scripts/install.sh
@@ -49,7 +49,7 @@ arch=$(uname -m)
 tmpdir="$HOME/temp"
 
 # Cron related
-croncmd="geoipupdate -d /etc/nginx/geoip >> $HOME/geoipupdate.log 2>&1"
+croncmd="sh -c \"date '+%Y-%m-%d %H:%M:%S' >> /home/servadmin/geoipupdate.log 2>&1 && geoipupdate -v -d /etc/nginx/geoip >> /home/servadmin/geoipupdate.log 2>&1\""
 cronjob="0 0 * * * $croncmd"
 
 # stdout and stderr for commands logged
